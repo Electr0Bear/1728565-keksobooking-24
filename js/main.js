86528c3bd09327ac2bbe65ae0d0c9ccb90c48d7d
Electr0Bear <dmitry.kn@protonmail.com>
2021-10-29 17:33:42 +0300

    убирает switch и переносит типы жилья в отдельный объект

--- js/main.js
+++ js/main.js
@@ -44,45 +44,27 @@ const getPosts = (uniqueObjects) => {
 
   const getRndElements = array => [... new Set (Array.from({length: getRndInteger(1, array.length)}, () => array[getRndInteger(0, array.length - 1)]))];
 
+  const types = {
+    'palace': 'ый ' + TITLE_2[0],
+    'flat': 'ая ' + TITLE_2[1],
+    'house': 'ый ' + TITLE_2[2],
+    'bungalow': 'ое ' + TITLE_2[3],
+    'hotel': 'ый ' + TITLE_2[4],
+  }
+
   // Основной цикл генерации объекта
   for (let i = 0; i < uniqueObjects; i++) {
 
     const latitude = getRndFloat(35.65000, 35.70000, 5);
     const longitude = getRndFloat(139.70000, 139.80000, 5);
     const type = TYPES[getRndInteger(0, 4)];
-    let rooms;
-    let title = TITLE_1[getRndInteger(0, TITLE_1.length - 1)];
-    switch (type) {
-      case 'palace':
-        rooms = getRndInteger(10, 20);
-        title += 'ый ' + TITLE_2[0];
-        break;
-      case 'flat':
-        rooms = getRndInteger(1, 4);
-        title += 'ая ' + TITLE_2[1];
-        break;
-      case 'house':
-        rooms = getRndInteger(1, 6);
-        title += 'ый ' + TITLE_2[2];
-        break;
-      case 'bungalow':
-        rooms = getRndInteger(1, 2);
-        title += 'ое ' + TITLE_2[3];
-        break;
-      case 'hotel':
-        rooms = getRndInteger(1, 2);
-        title += 'ый ' + TITLE_2[4];
-        break;
-      default: rooms = 1;
-    }
+    let rooms = getRndInteger(1, 10);
+    let title = TITLE_1[getRndInteger(0, TITLE_1.length - 1)] + types[type];
+
     const price = rooms * getRndInteger(1000, 5000);
     const guests = rooms * getRndInteger(1, 3);
     const checkin = CHECK_IN_OUT_TIME[getRndInteger(0, CHECK_IN_OUT_TIME.length - 1)];
     const checkout = CHECK_IN_OUT_TIME[getRndInteger(0, CHECK_IN_OUT_TIME.indexOf(checkin))];
-
-    // const features = getRndElements(FEATURES);
-    // const description = getRndElements(DESCRIPTION);
-    // const photos = getRndElements(PHOTOS);
 
     post.push({
       author: {
