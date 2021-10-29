d5aafcee2c91c5dfdf6f178b1bf1536aa6a89291
Electr0Bear <dmitry.kn@protonmail.com>
2021-10-30 02:11:57 +0300

    выносит создание объекта в отдельную функцию и создает функцию-цикл для создания массива объектов

--- js/main.js
+++ js/main.js
@@ -5,11 +5,18 @@ const CHECK_IN_OUT_TIME = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const DESCRIPTION = ['Новый ремонт', 'Живописный вид', 'Тихий район', 'В шаговой доступности от метро', 'Можно с животными', 'Удобный паркинг', 'Рядом есть парк'];
 const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const types = {
+  'palace': 'ый ' + TITLE_2[0],
+  'flat': 'ая ' + TITLE_2[1],
+  'house': 'ый ' + TITLE_2[2],
+  'bungalow': 'ое ' + TITLE_2[3],
+  'hotel': 'ый ' + TITLE_2[4],
+};
 
 // Функция возвращающая случайное целое число в заданном диапазоне
-const getRndInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+const getRndInteger = (numA, numB) => {
+  const lower = Math.ceil(Math.min(Math.abs(numA), Math.abs(numB)));
+  const upper = Math.floor(Math.max(Math.abs(numA), Math.abs(numB)));
 
   const result = Math.random() * (upper - lower + 1) + lower;
 
@@ -17,81 +24,70 @@ const getRndInteger = (a, b) => {
 };
 
 // Функция возвращающая случайное число в заданном диапазоне с указанием количества знаков после запятой
-const getRndFloat = (a, b, digits = 1) => {
-  const lower = Math.min(Math.abs(a), Math.abs(b));
-  const upper = Math.max(Math.abs(a), Math.abs(b));
+const getRndFloat = (numA, numB, digits = 1) => {
+  const lower = Math.min(Math.abs(numA), Math.abs(numB));
+  const upper = Math.max(Math.abs(numA), Math.abs(numB));
 
   const result = Math.random() * (upper - lower) + lower;
 
   return result.toFixed(digits);
 };
 
+// Функиця, возвращающая массив из случаного набора уникальных элементов от передаваемого массива
+const getRndElements = array => [...new Set(Array.from({length: getRndInteger(1, array.length)}, () => array[getRndInteger(0, array.length - 1)]))];
 
-const getPosts = (uniqueObjects) => {
-  const post = [];
 
-  // Генерирует числа под ссылки на аватары авторов
-  const rndIntArray = [];
-  while (rndIntArray.length < uniqueObjects) {
-    let rndInt = '' + getRndInteger(1, uniqueObjects);
-    if (rndInt < 10) {
-      rndInt = '0' + rndInt;
-    }
-    if (rndIntArray.indexOf(rndInt) === -1) {
-      rndIntArray.push(rndInt);
-    }
-  }
+// Функия генерации объекта
+const getObject = (counter) => {
+  const avatar = 'img/avatars/user' + counter + '.png';
+  const latitude = getRndFloat(35.65000, 35.70000, 5);
+  const longitude = getRndFloat(139.70000, 139.80000, 5);
+  const type = TYPES[getRndInteger(0, 4)];
+  const rooms = getRndInteger(1, 10);
+  const title = TITLE_1[getRndInteger(0, TITLE_1.length - 1)] + types[type];
+  const price = rooms * getRndInteger(1000, 5000);
+  const guests = rooms * getRndInteger(1, 3);
+  const checkin = CHECK_IN_OUT_TIME[getRndInteger(0, CHECK_IN_OUT_TIME.length - 1)];
+  const checkout = CHECK_IN_OUT_TIME[getRndInteger(0, CHECK_IN_OUT_TIME.indexOf(checkin))];
 
-  const getRndElements = array => [... new Set (Array.from({length: getRndInteger(1, array.length)}, () => array[getRndInteger(0, array.length - 1)]))];
+  const post = {
+    author: {
+      avatar: avatar,
+    },
 
-  const types = {
-    'palace': 'ый ' + TITLE_2[0],
-    'flat': 'ая ' + TITLE_2[1],
-    'house': 'ый ' + TITLE_2[2],
-    'bungalow': 'ое ' + TITLE_2[3],
-    'hotel': 'ый ' + TITLE_2[4],
+    offer: {
+      title: title,
+      address: latitude + ', ' + longitude,
+      price: price,
+      type: type,
+      rooms: rooms,
+      guests: guests,
+      checkin: checkin,
+      checkout: checkout,
+      features: getRndElements(FEATURES),
+      description: getRndElements(DESCRIPTION),
+      photos: getRndElements(PHOTOS),
+    },
+
+    location: {
+      lat: latitude,
+      lng: longitude,
+    },
   };
-
-  // Основной цикл генерации объекта
-  for (let i = 0; i < uniqueObjects; i++) {
-
-    const latitude = getRndFloat(35.65000, 35.70000, 5);
-    const longitude = getRndFloat(139.70000, 139.80000, 5);
-    const type = TYPES[getRndInteger(0, 4)];
-    let rooms = getRndInteger(1, 10);
-    let title = TITLE_1[getRndInteger(0, TITLE_1.length - 1)] + types[type];
-
-    const price = rooms * getRndInteger(1000, 5000);
-    const guests = rooms * getRndInteger(1, 3);
-    const checkin = CHECK_IN_OUT_TIME[getRndInteger(0, CHECK_IN_OUT_TIME.length - 1)];
-    const checkout = CHECK_IN_OUT_TIME[getRndInteger(0, CHECK_IN_OUT_TIME.indexOf(checkin))];
-
-    post.push({
-      author: {
-        avatar: 'img/avatars/user' + rndIntArray[i] + '.png',
-      },
-
-      offer: {
-        title: title,
-        address: latitude + ', ' + longitude,
-        price: price,
-        type: type,
-        rooms: rooms,
-        guests: guests,
-        checkin: checkin,
-        checkout: checkout,
-        features: getRndElements(FEATURES),
-        description: getRndElements(DESCRIPTION),
-        photos: getRndElements(PHOTOS),
-      },
-
-      location: {
-        lat: latitude,
-        lng: longitude,
-      },
-    });
-  }
   return post;
 };
 
-console.log(getPosts(10));
+const generateObjects = numOfObjects => {
+  const postsArr = [];
+
+  for (let i = 1; i <= numOfObjects; i++) {
+    let currentObj = '' + i;
+
+    i < 10 ? currentObj = '0' + currentObj : currentObj;
+
+    postsArr.push(getObject(currentObj));
+  }
+  return postsArr;
+};
+
+console.log(generateObjects(4));
