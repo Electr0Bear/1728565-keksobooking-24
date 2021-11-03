f8c9025b4e8b91fff2888d0e5321b8ec46132e9f
Electr0Bear <dmitry.kn@protonmail.com>
2021-11-03 23:44:19 +0300

    возвращает generateObjects и вызывает generatePost с первым элементом generateObjects

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 import {generatePost} from './advertisment.js';
 import {getObject} from './data.js';
 
-const generatePosts = (numOfObjects) => {
+const generateObjects = (numOfObjects) => {
   const postsArr = [];
 
   for (let count = 1; count <= numOfObjects; count++) {
-    postsArr.push(generatePost(getObject(count)));
+    postsArr.push(getObject(count));
   }
   return postsArr;
 };
 
-generatePosts(1);
+const generatedObject = generateObjects(1);
+generatePost(generatedObject[0]);
