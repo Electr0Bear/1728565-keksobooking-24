7388a426cac2ab38151674a2981fcf5e242431eb
Electr0Bear <dmitry.kn@protonmail.com>
2021-11-04 00:36:47 +0300

    удаляет структуру с изменением окончаний слов комнат и гостей, удаляет проверку на пустоту элементов разметки

--- js/advertisment.js
+++ js/advertisment.js
@@ -17,13 +17,7 @@ const generatePost = (object) => {
   newPost.querySelector('.popup__text--address').textContent = offer.address;
   newPost.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
   newPost.querySelector('.popup__type').textContent = typeOfEstate[offer.type];
-  const capacity = newPost.querySelector('.popup__text--capacity');
-  offer.rooms === 1 ?
-    capacity.textContent = `${offer.rooms} комнатa для ${offer.guests} гостей`
-    : capacity.textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-  if (offer.rooms >= 5) {
-    capacity.textContent = `${offer.rooms} комнат для ${offer.guests} гостей`;
-  }
+  newPost.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнат для ${offer.guests} гостей`;
   newPost.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
 
   const features = offer.features;
@@ -47,12 +41,6 @@ const generatePost = (object) => {
   for (let iterator = 0; iterator < postPhotosArray.length; iterator++) {
     postPhotosArray[iterator].src = `${photos[iterator]}`;
   }
-
-  // Array.from(newPost.children).forEach((child) => {
-  //   if (child.innerHTML.trim() === '') {
-  //     child.remove();
-  //   }
-  // });
 
   map.appendChild(newPost);
 };
