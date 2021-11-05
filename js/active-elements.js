ac078f7b0b0b0be4698c369ac2500fadadefc325
Electr0Bear <dmitry.kn@protonmail.com>
2021-11-05 20:39:07 +0300

    убирает квадратные скобки

--- js/active-elements.js
+++ js/active-elements.js
@@ -4,8 +4,8 @@ const mapForm = document.querySelector('.map__filters');
 const mapFilters = document.querySelectorAll('.map__filter, .map__features, .map__checkbox');
 
 const setStatus = (status) => {
-  adForm.classList.toggle('ad-form--disabled' [!status]);
-  mapForm.classList.toggle('map__filters--disabled' [!status]);
+  adForm.classList.toggle('ad-form--disabled',!status);
+  mapForm.classList.toggle('map__filters--disabled',!status);
 
   adFieldsets.forEach((filter) => {
     filter.disabled = !status;
