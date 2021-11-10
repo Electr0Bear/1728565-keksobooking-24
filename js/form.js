5f4ecdd0748b88fabee5052c882297458d7ffa28
Electr0Bear <dmitry.kn@protonmail.com>
2021-11-10 22:17:18 +0300

    добавляет изменение минимальной цены и плейсхолдера в инпут СТОИМОСТИ

--- js/form.js
+++ js/form.js
@@ -1,6 +1,13 @@
 const title = document.querySelector('#title');
 const propertyType = document.querySelector('#type');
 const price = document.querySelector('#price');
+const propertyPrices = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
 
 title.addEventListener('invalid', () => {
   if (title.validity.tooShort) {
@@ -14,14 +21,28 @@ title.addEventListener('invalid', () => {
   }
 });
 
-price.addEventListener('invalid', () => {
-  if (price.validity.valueMissing) {
-    price.setCustomValidity('Обязательное поле');
-  } else if (price.validity.rangeUnderflow) {
+price.addEventListener('input', () => {
+  if (price.validity.rangeUnderflow) {
     price.setCustomValidity(`Для выбранного типа жилья стоимость не может быть меньше ${price.min}`);
   } else if (price.validity.rangeOverflow) {
     price.setCustomValidity('Стоимость не должна превышать 1 000 000');
   } else {
     price.setCustomValidity('');
   }
+
+  price.reportValidity();
+});
+
+price.addEventListener('invalid', () => {
+  if (price.validity.valueMissing) {
+    price.setCustomValidity('Обязательное поле');
+  } else if (!price.validity) {
+    price.setCustomValidity('');
+  }
+});
+
+propertyType.addEventListener('change', () => {
+  const currentType = propertyType.value;
+  price.min = propertyPrices[currentType];
+  price.placeholder = propertyPrices[currentType];
 });
