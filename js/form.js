57f6025dcd007e4bdb08651d6a106e9e15b304b9
Electr0Bear <dmitry.kn@protonmail.com>
2021-11-11 10:56:31 +0300

    удаляет лишнюю проверку на превышение максимальной длины строки в ЗАГОЛОВКЕ

--- js/form.js
+++ js/form.js
@@ -17,8 +17,6 @@ title.addEventListener('invalid', () => {
   title.setCustomValidity('');
   if (title.validity.tooShort) {
     title.setCustomValidity('Заголовок должен состоять минимум из 20 символов');
-  } else if (title.validity.tooLong) {
-    title.setCustomValidity('Заголовок не должен превышать 100 символов');
   } else if (title.validity.valueMissing) {
     title.setCustomValidity('Обязательное поле');
   }
