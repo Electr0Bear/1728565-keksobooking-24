71318289235de60593b69f1515b069756a1ece60
Electr0Bear <dmitry.kn@protonmail.com>
2021-11-15 23:11:04 +0300

    добавляет фичи и фотографии в балуны, убирает лишний закомменченый код

--- js/mapbaloon.js
+++ js/mapbaloon.js
@@ -19,9 +19,40 @@ const createBaloon = (object) => {
   post.querySelector('.popup__type').textContent = typeOfEstate[offer.type];
   post.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнат для ${offer.guests} гостей`;
   post.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+
+  const features = offer.features;
+  const featureList = post.querySelectorAll('.popup__feature');
+  if (!features) {
+    post.querySelector('.popup__features').remove();
+  } else {
+    featureList.forEach((featureItem) => {
+      const activeFeatureItem = features.some((feature) =>
+        featureItem.classList.contains(`popup__feature--${feature}`));
+      if (!activeFeatureItem) {
+        featureItem.remove();
+      }
+    });
+  }
+
   post.querySelector('.popup__description').textContent = offer.description;
+
+  const postPhotosSection = post.querySelector('.popup__photos');
+  const photos = offer.photos;
+  if (!photos) {
+    post.querySelector('.popup__photos').remove();
+  } else {
+    for (let iterator = 1; iterator < photos.length; iterator++) {
+      postPhotosSection.appendChild(post.querySelector('.popup__photo').cloneNode(true));
+    }
+    const postPhotosArray = post.querySelectorAll('.popup__photo');
+    for (let iterator = 0; iterator < postPhotosArray.length; iterator++) {
+      postPhotosArray[iterator].src = `${photos[iterator]}`;
+    }
+  }
 
   return post;
 };
 
-export {createBaloon};
+export {
+  createBaloon
+};
