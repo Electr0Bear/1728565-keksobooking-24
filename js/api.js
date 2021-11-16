45354cb8e55c2ba8b85a9737ad2288bfafb9d462
Electr0Bear <dmitry.kn@protonmail.com>
2021-11-17 00:38:14 +0300

    переносить onError, onGoodSubmit, onFailedSubmit в api

--- js/api.js
+++ js/api.js
@@ -1,4 +1,34 @@
-import {onError, onGoodSubmit, onFailedSubmit} from './util.js';
+import {closePopup} from './util.js';
+import {putUserBaloon} from './map.js';
+
+const onError = (err) => {
+  const errorPopup = `<div class="error">
+    <p style="color: #FFF">Не удалось загрузить данные с сервера.<br>Код ошибки: ${err}</p>
+    <button type="button" class="error__button-onload" style="color: white; border: 4px solid #ff5635; background-color: #ff5635; border-radius: 8px;">OK</button>
+    </div>`;
+  document.querySelector('body').insertAdjacentHTML('beforeend', errorPopup);
+  document.querySelector('.error__button-onload').addEventListener('click', () =>
+    document.querySelector('.error').remove(),
+  );
+  closePopup(errorPopup);
+};
+
+const onGoodSubmit = (post) => {
+  putUserBaloon(post);
+  document.querySelector('.ad-form__reset').click();
+  const successPopupTemplate = document.querySelector('#success').content.querySelector('.success');
+  const successPopup = successPopupTemplate.cloneNode(true);
+  document.querySelector('body').appendChild(successPopup);
+  closePopup(successPopup);
+};
+
+const onFailedSubmit = () => {
+  document.querySelector('.ad-form__reset').click();
+  const failurePopupTemplate = document.querySelector('#error').content.querySelector('.error');
+  const failurePopup = failurePopupTemplate.cloneNode(true);
+  document.querySelector('body').appendChild(failurePopup);
+  closePopup(failurePopup);
+};
 
 const getData = (onSuccess) => {
   fetch('https://24.javascript.pages.academy/keksobooking/data')
--- js/util.js
+++ js/util.js
@@ -1,5 +1,3 @@
-import {putUserBaloon} from './map.js';
-
 const closePopup = (element) => {
   element.addEventListener('click', (evt) => {
     evt.preventDefault();
@@ -14,33 +12,5 @@ const closePopup = (element) => {
   });
 };
 
-const onError = (err) => {
-  const errorPopup = `<div class="error">
-    <p style="color: #FFF">Не удалось загрузить данные с сервера.<br>Код ошибки: ${err}</p>
-    <button type="button" class="error__button-onload" style="color: white; border: 4px solid #ff5635; background-color: #ff5635; border-radius: 8px;">OK</button>
-    </div>`;
-  document.querySelector('body').insertAdjacentHTML('beforeend', errorPopup);
-  document.querySelector('.error__button-onload').addEventListener('click', () =>
-    document.querySelector('.error').remove(),
-  );
-  closePopup(errorPopup);
-};
 
-const onGoodSubmit = (post) => {
-  putUserBaloon(post);
-  document.querySelector('.ad-form__reset').click();
-  const successPopupTemplate = document.querySelector('#success').content.querySelector('.success');
-  const successPopup = successPopupTemplate.cloneNode(true);
-  document.querySelector('body').appendChild(successPopup);
-  closePopup(successPopup);
-};
-
-const onFailedSubmit = () => {
-  document.querySelector('.ad-form__reset').click();
-  const failurePopupTemplate = document.querySelector('#error').content.querySelector('.error');
-  const failurePopup = failurePopupTemplate.cloneNode(true);
-  document.querySelector('body').appendChild(failurePopup);
-  closePopup(failurePopup);
-};
-
-export {onError, onGoodSubmit, onFailedSubmit};
+export {closePopup};
