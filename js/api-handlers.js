45fbca2227e822e7fa5d208e993ddf1698199644
Electr0Bear <dmitry.kn@protonmail.com>
2021-11-17 11:24:41 +0300

    создаёт переменную pageBody в api-handlers

--- js/api-handlers.js
+++ js/api-handlers.js
@@ -1,11 +1,13 @@
 import {closePopup, resetForm} from './util.js';
 
+const pageBody = document.querySelector('body');
+
 const onError = (err) => {
   const errorPopupElement = `<div class="error">
     <p style="color: #FFF">Не удалось загрузить данные с сервера.<br>Код ошибки: ${err}</p>
     <button type="button" class="error__button-onload" style="color: white; border: 4px solid #ff5635; background-color: #ff5635; border-radius: 8px;">OK</button>
     </div>`;
-  document.querySelector('body').insertAdjacentHTML('beforeend', errorPopupElement);
+  pageBody.insertAdjacentHTML('beforeend', errorPopupElement);
   const errorPopup = document.querySelector('.error');
   document.querySelector('.error__button-onload').addEventListener('click', () =>
     errorPopup.remove(),
@@ -17,7 +19,7 @@ const onSuccessSubmit = () => {
   resetForm();
   const successPopupTemplate = document.querySelector('#success').content.querySelector('.success');
   const successPopup = successPopupTemplate.cloneNode(true);
-  document.querySelector('body').appendChild(successPopup);
+  pageBody.appendChild(successPopup);
   closePopup(successPopup);
 };
 
@@ -25,7 +27,7 @@ const onFailedSubmit = () => {
   resetForm();
   const failurePopupTemplate = document.querySelector('#error').content.querySelector('.error');
   const failurePopup = failurePopupTemplate.cloneNode(true);
-  document.querySelector('body').appendChild(failurePopup);
+  pageBody.appendChild(failurePopup);
   closePopup(failurePopup);
 };
 
